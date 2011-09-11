27689e909afda5852a11e3bd835bb449e9216c56
Gawdl3y@gmail.com <Gawdl3y@gmail.com@f07e0a44-1623-f050-27e3-da6d326f8dd3>
2011-09-11 19:14:30 +0000

    changed the way times are stored, added goal minutes

--- chrome/scripts/task-control.js
+++ chrome/scripts/task-control.js
@@ -11,7 +11,7 @@ function add_task(data) {
 function reset_task(task) {
     if(localStorage['confirm-reset'] == 'false' || confirm('Are you sure you want to reset task "'+ tasks[task].text +'"?')) {
         tasks[task].current = 0;
-        $('#task-'+ task +' td.current').text(format_time(tasks[task].current));
+        $('#task-'+ task +' td.current').text(format_time(tasks[task].current_hours, tasks[task].current_mins, tasks[task].current_secs));
         $('#task-'+ task +' progress').val(0).text('0%');
         $('#task-'+ task +' button.toggle').removeAttr('disabled');
         if(!task_running[task]) $('#task-'+ task).removeAttr('class');
@@ -73,24 +73,18 @@ function toggle_task(task) {
 // Add the task to the list
 function list_task(task, anim) {
     // Progress done
-    var progress = Math.floor(tasks[task].current / tasks[task].goal * 100);
+    var progress = Math.floor((tasks[task].current_hours + (tasks[task].current_mins / 60) + (tasks[task].current_secs / 3600)) / (tasks[task].goal_hours + (tasks[task].goal_mins / 60)) * 100);
     
     // Create the row
     $('#row-template').clone().attr('id', 'task-'+ task).appendTo('#task-list tbody');
     
     // Text
     $('#task-'+ task +' td.text').text(tasks[task].text);
-    $('#task-'+ task +' td.current').text(format_time(tasks[task].current));
-    $('#task-'+ task +' td.goal').text(tasks[task].goal.toString() + (tasks[task].goal > 1 ? ' Hours' : ' Hour'));
+    $('#task-'+ task +' td.current').text(format_time(tasks[task].current_hours, tasks[task].current_mins, tasks[task].current_secs));
+    $('#task-'+ task +' td.goal').text(format_time(tasks[task].goal_hours, tasks[task].goal_mins, 0));
     $('#task-'+ task +' button.toggle').text(task_running[task] ? 'Stop' : 'Start');
     $('#task-'+ task +' progress').val(progress).text(progress.toString() +'%');
     if(task_running[task]) $('#task-'+ task).attr('class', 'running');
-    
-    // Disable the toggle button if task is at its goal, and change the bg colour
-    if(tasks[task].current >= tasks[task].goal - 0.00015) {
-        $('#task-'+ task +' button.toggle').attr('disabled', 'disabled');
-        $('#task-'+ task).attr('class', 'done');
-    }
     
     // Option Buttons
     $('#task-'+ task +' button.toggle').attr('name', task).click(function() {
@@ -102,6 +96,12 @@ function list_task(task, anim) {
     $('#task-'+ task +' button.delete').attr('name', task).click(function() {
         delete_task(parseInt(this.name));
     });
+    
+    // Disable the toggle button if task is at its goal, and change the bg colour
+    if(tasks[task].current_hours >= tasks[task].goal_hours && tasks[task].current_mins >= tasks[task].goal_mins) {
+        $('#task-'+ task +' button.toggle').attr('disabled', 'disabled');
+        $('#task-'+ task).attr('class', 'done');
+    }
     
     // Animation
     if(anim == 0) {
@@ -126,11 +126,12 @@ function update_time() {
     for(i = 0; i < task_count; i++) {
         if(task_running[i]) {
             // Increment time
-            tasks[i].current += (1 / 3600) * parseInt(localStorage['update-time']);
+            tasks[i].current_secs++;
+            if(tasks[i].current_secs >= 60) { tasks[i].current_secs = 0; tasks[i].current_mins++; }
+            if(tasks[i].current_mins >= 60) { tasks[i].current_mins = 0; tasks[i].current_hours++; }
             
             // Stop updating this one if it's at the goal, show a desktop notification, and play the sound
-            if(tasks[i].current >= tasks[i].goal - 0.00015) {
-                tasks[i].current = tasks[i].goal;
+            if(tasks[i].current_hours >= tasks[i].goal_hours && tasks[i].current_mins >= tasks[i].goal_mins) {
                 toggle_task(i);
                 $('#task-'+ i +' button.toggle').attr('disabled', 'disabled');
                 $('#task-'+ i).attr('class', 'done');
@@ -141,10 +142,10 @@ function update_time() {
                 }
             }
             
-            var progress = Math.floor(tasks[i].current / tasks[i].goal * 100);
+            var progress = Math.floor((tasks[i].current_hours + (tasks[i].current_mins / 60) + (tasks[i].current_secs / 3600)) / (tasks[i].goal_hours + (tasks[i].goal_mins / 60)) * 100);
             
             // Update list
-            $('#task-'+ i +' td.current').text(format_time(tasks[i].current));
+            $('#task-'+ i +' td.current').text(format_time(tasks[i].current_hours, tasks[i].current_mins, tasks[i].current_secs));
             $('#task-'+ i +' progress').val(progress).text(progress.toString() +'%')
         }
     }
