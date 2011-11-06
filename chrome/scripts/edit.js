e8d9d27758eb79dff60bdd8d7d21a39218214ca8
Gawdl3y@gmail.com <Gawdl3y@gmail.com@f07e0a44-1623-f050-27e3-da6d326f8dd3>
2011-11-06 22:34:32 +0000

    bug fixes, style changes, indefinite goal option

--- chrome/scripts/edit.js
+++ chrome/scripts/edit.js
@@ -1,4 +1,4 @@
-var editing_task = -1;
+var editing_task = -1, was_running;
 
 // Begin editing a task's name
 function edit_name(task) {
@@ -26,29 +26,34 @@ function edit_name(task) {
             cancel_edit();
         });
     } else {
-        if(task != editing_task) alert('Finish editing any other task first!');
+        if(task != editing_task) alert('Finish editing task "'+ tasks[editing_task].text +'" first!');
     }
 }
 
 // Finish editing a task's name
 function save_name(task) {
-    // Set the name
-    tasks[task].text = $('#name-edit-'+ task +' input').val();
-    
-    // Replace the edit fields with just the task name
-    $('#task-'+ task +' td.text').empty().text(tasks[task].text);
-    
-    // Enable all delete buttons and the task's toggle button
-    $('button.delete, #task-'+ task +' button.toggle').removeAttr('disabled');
-    
-    editing_task = -1;
-    save();
+    if($('#name-edit-'+ task +' input').val() != '') {
+        // Set the name
+        tasks[task].text = $('#name-edit-'+ task +' input').val();
+        
+        // Replace the edit fields with just the task name
+        $('#task-'+ task +' td.text').empty().text(tasks[task].text);
+        
+        // Enable all delete buttons and the task's toggle button
+        $('button.delete, #task-'+ task +' button.toggle').removeAttr('disabled');
+        
+        editing_task = -1;
+        save();
+    } else {
+        $('#error').fadeIn(600).delay(2000).fadeOut(600);
+    }
 }
 
 // Begin editing a task's goal
 function edit_goal(task) {
     if(editing_task === -1) {
         editing_task = task;
+        was_running = task_running[task];
         if(task_running[task]) toggle_task(task);
         
         // Disable the task's toggle button
@@ -58,42 +63,66 @@ function edit_goal(task) {
         $('#task-'+ task +' td.goal').empty();
         $('#goal-edit-template').clone().attr('id', 'goal-edit-'+ task).appendTo('#task-'+ task +' td.goal');
         
-        // Set the current goal and focus
-        $('#goal-edit-'+ task +' input').first().val(tasks[task].goal_hours).focus();
-        $('#goal-edit-'+ task +' input').last().val(tasks[task].goal_mins);
-        
-        // Set names & add events
-        $('#goal-edit-'+ task +' input').attr('name', task).keypress(function (e) {
-            if(e.keyCode == 13) save_goal(parseInt(this.name));
+        // Add events
+        $('#goal-edit-'+ task +' input').keypress(function (e) {
+            if(e.keyCode == 13) save_goal(editing_task);
+        })
+        $('#goal-edit-'+ task +' .hrs, #goal-edit-'+ task +'.mins').blur(function() {
+            if($(this).val() == '') $(this).val('0');
         });
+        
+        $('#goal-edit-'+ task +' .indef').change(function() {
+            if($(this).is(':checked')) {
+                $('.hrs, .mins').attr('disabled', 'disabled');
+            } else {
+                $('.hrs, .mins').removeAttr('disabled');
+            }
+        });
+        
         $('#goal-edit-'+ task +' button.save').attr('name', task).click(function() {
             save_goal(parseInt(this.name));
         });
         $('#goal-edit-'+ task +' button.cancel').click(function() {
             cancel_edit();
         });
+        
+        // Set the current goal and focus
+        $('#goal-edit-'+ task +' .hrs').val(tasks[task].goal_hours).focus();
+        $('#goal-edit-'+ task +' .mins').val(tasks[task].goal_mins);
+        if(tasks[task].indefinite) {
+            $('#goal-edit-'+ task +' .indef').attr('checked', 'checked').focus();
+            $('#goal-edit-'+ task +' .hrs, #goal-edit-'+ task +' .mins').attr('disabled', 'disabled');
+        }
     } else {
-        if(task != editing_task) alert('Finish editing any other task first!');
+        if(task != editing_task) alert('Finish editing task "'+ tasks[editing_task].text +'" first!');
     }
 }
 
 // Finish editing a task's goal
 function save_goal(task) {
-    // Set the goal
-    tasks[task].goal_hours = parseInt($('#goal-edit-'+ task +' input').first().val());
-    tasks[task].goal_mins = parseInt($('#goal-edit-'+ task +' input').last().val());
-    
-    // Replace the edit fields with just the task goal
-    $('#task-'+ task +' td.goal').empty().text(format_time(tasks[task].goal_hours, tasks[task].goal_mins, 0));
-    
-    // Rebuild task list
-    $('#task-list tbody').empty();
-    for(i = 0; i < task_count; i++) {
-        list_task(i, 0);
+    if($('#goal-edit-'+ task +' .hrs').val() == '') $('#goal-edit-'+ task +' .hrs').val('0');
+    if($('#goal-edit-'+ task +' .mins').val() == '') $('#goal-edit-'+ task +' .mins').val('0');
+        
+    var hours = parseInt($('#goal-edit-'+ task +' .hrs').val()), mins = parseInt($('#goal-edit-'+ task +' .mins').val()), indef = $('#goal-edit-'+ task +' .indef').is(':checked');
+        
+    if((hours > 0 || mins > 0 || indef) && !(hours < 0) && !(mins < 0)) {
+        // Set the goal
+        tasks[task].goal_hours = hours;
+        tasks[task].goal_mins = mins;
+        tasks[task].indefinite = indef;
+        
+        // Rebuild task list
+        $('#task-list tbody').empty();
+        for(i = 0; i < task_count; i++) {
+            list_task(i, 0);
+        }
+        
+        editing_task = -1;
+        save();
+        if(was_running) toggle_task(task);
+    } else {
+        $('#error').fadeIn(600).delay(2000).fadeOut(600);
     }
-    
-    editing_task = -1;
-    save();
 }
 
 // Cancel editing
