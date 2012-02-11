ec2884b3cb047048304392f3b688de4f2e49823b
Gawdl3y@gmail.com <Gawdl3y@gmail.com@f07e0a44-1623-f050-27e3-da6d326f8dd3>
2012-02-11 21:53:18 +0000

    made \n in messages switch to <br /> in HTML i18ns

--- trunk/chrome/scripts/utilities.js
+++ trunk/chrome/scripts/utilities.js
@@ -69,7 +69,7 @@ function localisePage() {
         var i18n = locale($(this).attr('i18n'));
         
         if($.inArray($(this)[0].tagName, text_tags) != -1) $(this).text(i18n);
-        else if($.inArray($(this)[0].tagName, html_tags) != -1) $(this).html(i18n);
+        else if($.inArray($(this)[0].tagName, html_tags) != -1) $(this).html(i18n.replace('\n', '<br />'));
         
         if($(this).attr('title')) $(this).attr('title', i18n);
         if($(this).attr('alt')) $(this).attr('alt', i18n);
