0f371f1d8cbe4d49bae5cfe58766d3f9c8406cc3
Gawdl3y@gmail.com <Gawdl3y@gmail.com@f07e0a44-1623-f050-27e3-da6d326f8dd3>
2012-03-24 18:30:53 +0000

    added translate notice made jQuery center 2.0

--- trunk/chrome/scripts/main.js
+++ trunk/chrome/scripts/main.js
@@ -13,26 +13,29 @@ $(document).ready(function() {
         // Set some variables
         load = $('#loading');
         version = chrome.app.getDetails().version;
-        
+
         // Localise the page
         localisePage();
-        
+
+        // Show translations text
+        if(window.navigator.language != 'en' && window.navigator.language != 'en-GB' && window.navigator.language != 'en-CA' && window.navigator.language != 'en-US') $('#translations').show();
+
         // Retrieve any tasks they've previously added
         if(localStorage['tasks']) {
             tasks = JSON.parse(localStorage['tasks']);
             task_count = tasks.length;
-            
+
             for(i = 0; i < task_count; i++) {
                 // Convert from the old method of storing times to the new one
                 if(typeof tasks[i].current_hours == 'undefined') {
                     tasks[i].current_hours = Math.floor(tasks[i].current);
                     tasks[i].current_mins = Math.floor((tasks[i].current - tasks[i].current_hours) * 60);
                     tasks[i].current_secs = Math.round((tasks[i].current - tasks[i].current_hours - (tasks[i].current_mins / 60)) * 3600);
-                    
+
                     tasks[i].goal_hours = Math.floor(tasks[i].goal);
                     tasks[i].goal_mins = Math.round((tasks[i].goal - tasks[i].goal_hours) * 60);
                 }
-                
+
                 // Add the notified property to a task if it doesn't exist
                 if(typeof tasks[i].notified == 'undefined') {
                     if(tasks[i].current_hours >= tasks[i].goal_hours && tasks[i].current_mins >= tasks[i].goal_mins) {
@@ -41,17 +44,17 @@ $(document).ready(function() {
                         tasks[i].notified = false;
                     }
                 }
-                
+
                 // Add the indefinite property to a task if it doesn't exist
                 if(typeof tasks[i].indefinite == 'undefined') tasks[i].indefinite = false;
 
                 // Add the description property to a task if it doesn't exist
                 if(typeof tasks[i].description == 'undefined') tasks[i].description = '';
-                
+
                 // Make sure goal times aren't null
                 if(tasks[i].goal_hours == null) tasks[i].goal_hours = 0;
                 if(tasks[i].goal_mins == null) tasks[i].goal_mins = 0;
-                
+
                 list_task(i, 0);
                 task_running[i] = false;
             }
@@ -64,7 +67,7 @@ $(document).ready(function() {
                 Setting('stop-timer', true);
             }
         }
-        
+
         // Load settings
         LoadSettings();
 
@@ -82,17 +85,17 @@ $(document).ready(function() {
 
         // Set old-version to the current version
         localStorage['old-version'] = version;
-        
+
         // Enable the add task fields
         $('#new-task input, #new-task button').removeAttr('disabled');
-        
+
         // Check the auto-start box if enabled, and fill in the new task fields if enabled
         if(Setting('autostart-default')) $('#new-start').attr('checked', 'checked');
         if(Setting('save-fields')) {
             $('#new-txt').val(Setting('field-name', '', true));
             $('#new-goal-hours').val(Setting('field-hours', '4', true));
             $('#new-goal-mins').val(Setting('field-mins', '0', true));
-            
+
             if(Setting('field-start', false, true)) $('#new-start').attr('checked', 'checked');
             if(Setting('field-indef', false, true)) {
                 $('#new-goal-indef').attr('checked', 'checked');
@@ -100,17 +103,17 @@ $(document).ready(function() {
                 $('#new-goal-mins').attr('disabled', 'disabled');
             }
         }
-        
+
         // Set focus on the new task name field
         setTimeout(function() { $('#new-txt').focus(); }, 100);
-        
+
         // Start the timers
         timer = setTimeout('update_time()', Setting('update-time') * 1000);
         save_timer = setTimeout('SaveTasks(true)', 60000);
-        
+
         // Add to the launch count, and show a rating reminder if at a multiple of 6
         var launches = Setting('launches', Setting('launches', 0, true) + 1);
-        
+
         if(launches % 6 == 0 && typeof localStorage['rated'] == 'undefined') {
             dialog(locale('confRating'), function(status) {
                 if(status) {
@@ -119,44 +122,44 @@ $(document).ready(function() {
                 }
             }, {}, 'question');
         }
-        
+
         // Make the table rows draggable
         $('table#task-list').tableDnD({
             dragHandle: 'drag',
-            
+
             /*onDragStart: function(table, row) {
                 alert($(row).html());
                 var id = parseInt($(row).attr('id').replace('task-', ''));
                 dragging = tasks[id];
             },*/
-            
+
             onDrop: function(table, row) {
                 var old_id = parseInt($(row).attr('id').replace('task-', ''));
                 var id = $('table#task-list tbody tr').index(row);
                 var tmp = tasks[old_id], tmp2 = task_running[old_id];
-                
+
                 if(typeof tasks[old_id] != 'undefined' /*&& tasks[old_id] === dragging*/) {
                     tasks.splice(old_id, 1);
                     tasks.splice(id, 0, tmp);
                     task_running.splice(old_id, 1);
                     task_running.splice(id, 0, tmp2);
                 }
-                
+
                 rebuild_list();
             }
         });
-        
+
         // Add the history date picker
         $('#date-picker').datepicker({
             firstDay: 1,
             dateFormat: 'yy-mm-dd',
-            
+
             onSelect: function(dateText, inst) {
                 date = dateText.split('-');
                 show_history(parseInt(date[0]), parseInt(date[1]), parseInt(date[2]));
             }
         });
-            
+
         // Show and update everything
         $('div#tasks').show();
         tools_pulsate();
@@ -172,11 +175,11 @@ $(document).ready(function() {
 function rebuild_list() {
     editing_task = -1;
     $('table#task-list tbody').empty().removeClass('editing-name editing-current editing-goal');
-    
+
     for(i = 0; i < task_count; i++) {
         list_task(i, 0);
     }
-    
+
     $('table#task-list').tableDnDUpdate();
     rebuild_totals();
     rebuild_charts();
@@ -186,24 +189,24 @@ function rebuild_list() {
 function rebuild_totals() {
     if(task_count > 0) {
         var current_hours = 0, current_mins = 0, current_secs = 0, goal_hours = 0, goal_mins = 0, dec_current = 0, dec_this_current, dec_this_goal, progress, i;
-        
+
         // Get the total hours, minutes, and seconds
         for(i = 0; i < task_count; i++) {
             current_hours += tasks[i].current_hours;
             current_mins += tasks[i].current_mins;
             current_secs += tasks[i].current_secs;
-            
+
             if(!tasks[i].indefinite) {
                 goal_hours += tasks[i].goal_hours;
                 goal_mins += tasks[i].goal_mins;
-                
+
                 // Don't add excess time spent
                 dec_this_current = tasks[i].current_hours + (tasks[i].current_mins / 60) + (tasks[i].current_secs / 3600);
                 dec_this_goal = tasks[i].goal_hours + (tasks[i].goal_mins / 60);
                 dec_current += dec_this_current > dec_this_goal ? dec_this_goal : dec_this_current;
             }
         }
-        
+
         // Fix things like 12:72:142
         if(current_secs > 59) {
             current_mins += Math.floor(current_secs / 60);
@@ -217,16 +220,16 @@ function rebuild_totals() {
             goal_hours += Math.floor(goal_mins / 60);
             goal_mins %= 60;
         }
-        
+
         // Get the total progress done
         progress = Math.floor(dec_current / (goal_hours + (goal_mins / 60)) * 100);
         if(isNaN(progress)) progress = 0;
-        
+
         // Display
         $('table#task-list tfoot td.current').text(format_time(current_hours, current_mins, current_secs));
         $('table#task-list tfoot td.goal').text(format_time(goal_hours, goal_mins, 0));
         $('table#task-list tfoot progress').text(progress.toString() + '%').val(progress);
-        
+
         if(task_count >= 2) $('table#task-list tfoot').fadeIn(); else $('table#task-list tfoot').fadeOut();
     }
 }
@@ -235,15 +238,15 @@ function rebuild_totals() {
 function rebuild_charts() {
     if(Setting('enable-charts') && typeof tasks[0] != 'undefined') {
         var plot_data = new Array(), total_time = 0, i;
-        
+
         // Get the total of all times
         for(i = 0; i < task_count; i++) {
             total_time += (tasks[i].current_hours) + (tasks[i].current_mins / 60) + (tasks[i].current_secs / 3600);
         }
-        
+
         // Display charts container
         if(total_time > 0) $('#charts').fadeIn(); else $('#charts').fadeOut();
-        
+
         // Build the time spent chart
         for(i = 0; i < task_count; i++) {
             plot_data[i] = {
@@ -251,8 +254,8 @@ function rebuild_charts() {
                 data: ((tasks[i].current_hours) + (tasks[i].current_mins / 60) + (tasks[i].current_secs / 3600)) / total_time * 100
             };
         }
-        
-        
+
+
         // Display the time spent chart
         if(current_plot) {
             /*current_plot.setData(plot_data);
@@ -264,7 +267,7 @@ function rebuild_charts() {
                         show: true
                     }
                 },
-                
+
                 legend: {
                     show: false
                 }
@@ -276,7 +279,7 @@ function rebuild_charts() {
                         show: true
                     }
                 },
-                
+
                 legend: {
                     show: false
                 }
@@ -291,7 +294,7 @@ function rebuild_charts() {
 function verify_custom_sound(from_btn) {
     if($('#play-sound').attr('checked') && $('#sound-type').val() == 2) {
         var url = $('#custom-sound').val();
-        
+
         if(url.match(/^(((ht|f)tp(s?))\:\/\/).+$/i)) {
             // URLs! Interwebs!
             $('#custom-sound').removeClass('invalid');
@@ -304,7 +307,7 @@ function verify_custom_sound(from_btn) {
             // No format recognised
             $('#custom-sound').addClass('invalid');
         }
-        
+
         return false;
     } else {
         $('#custom-sound').removeClass('invalid');
@@ -319,7 +322,7 @@ function check_width() {
             Setting('use-icons', true);
             LoadSettings();
         }
-        
+
         Setting('small-window-alerted', true);
     }
 }
@@ -336,10 +339,10 @@ function tools_pulsate() {
 function SaveTasks(timeout) {
     if(timeout) load.show();
     $('button.delete, #new-btn').attr('disabled', 'disabled');
-    
+
     // Save task data
     localStorage['tasks'] = JSON.stringify(tasks);
-    
+
     // Save current new task field contents
     if(Setting('save-fields')) {
         Setting('field-name', $('#new-txt').val());
@@ -348,11 +351,11 @@ function SaveTasks(timeout) {
         Setting('field-indef', $('#new-goal-indef').is(':checked'));
         Setting('field-start', $('#new-start').is(':checked'));
     }
-    
+
     // Timeout
     clearTimeout(save_timer);
     save_timer = setTimeout('SaveTasks(true)', 60000);
-    
+
     $('button.delete, #new-btn').removeAttr('disabled');
     if(timeout) load.hide();
 }
