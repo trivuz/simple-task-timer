200311e7373743a1251bae22b24cf0397644b1d5
Gawdl3y@gmail.com <Gawdl3y@gmail.com@f07e0a44-1623-f050-27e3-da6d326f8dd3>
2012-03-25 01:00:37 +0000

    added window resolution to report bug page

--- chrome/scripts/report-bug.js
+++ chrome/scripts/report-bug.js
@@ -2,6 +2,16 @@ $(document).ready(function() {
 	localisePage();
 
 	// Print debug info
-    $('#debug-info').html('<strong>App Version:</strong> '+ chrome.app.getDetails().version +'<br />');
-    $('#debug-info').append('<strong>localStorage:</strong><br />'+ JSON.stringify(localStorage));
+	$('#debug-info')
+		.html('<strong>App Version:</strong> '+ chrome.app.getDetails().version +'<br />')
+		.append('<strong>localStorage:</strong><br />'+ JSON.stringify(localStorage))
+	;
+
+	// Resolution debug info
+	setTimeout(function() {
+		$('#debug-info')
+			.append('<br /><strong>Outer Resolution:</strong> '+ window.outerWidth +'x'+ window.outerHeight +'<br />')
+			.append('<strong>Inner Resolution:</strong> '+ window.innerWidth +'x'+ window.innerHeight)
+		;
+	}, 50);
 });
