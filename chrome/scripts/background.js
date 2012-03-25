1de2bc34493f84af87e1f602bcdc27119e910445
Gawdl3y@gmail.com <Gawdl3y@gmail.com@f07e0a44-1623-f050-27e3-da6d326f8dd3>
2012-03-25 05:29:10 +0000

    renamed index.html to main.html

--- chrome/scripts/background.js
+++ chrome/scripts/background.js
@@ -3,6 +3,6 @@ var opened = false, version = chrome.app.getDetails().version;
 // Open the app if it was updated
 if(typeof localStorage['old-version'] != 'undefined' &&  localStorage['old-version'] != version && (typeof localStorage['update-alert'] == 'undefined' || localStorage['update-alert'] == 'true')) {
     setTimeout(function() {
-    	if(!opened) window.open('index.html');
+    	if(!opened) window.open('main.html');
     }, 5000);
 }
